09745b6f890d243373fcc468deb3c1fd1106bfd1
zibaandy <zibaandy20@gmail.com>
2018-11-04 20:32:44 -0800

    todo commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
-import store from './store';
-import './App.css';
-import {observer} from 'mobx-react';
+import React, { Component } from "react";
+//  import "./App.css";
+import TodoEntry from "./components/TodoEntry";
+import TodoItms from "./components/TodoItems";
+import TodoFooter from "./components/TodoFooter";
+
+import { observer } from "mobx-react";
+
 @observer
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-       {store.time.toLocaleTimeString()}
-       <div>
-       <button
-       onClick={store.startClock()}
-       >
-         start
-       </button>
-       <button
-       onClick={store.pauseClock()}
-       >
-         pause
-       </button>
-       </div>
+      <div id="todoapp" className="todoapp">
+        <TodoEntry />
+        <TodoItms />
+        <TodoFooter />
       </div>
-      
     );
   }
 }
